dba5349ee30da0cce66ef318a5d3c21aed1914bd
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2026-01-13 17:59:03 +0100

    add initial server setup in index.ts

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import * as grpcLibrary from "@grpc/grpc-js";
+import server from "./app";
+import { PORT } from "./constants";
+
+// const bindAddress = `0.0.0.0:${PORT}`;
+const bindAddress = `127.0.0.1:${PORT}`;
+
+function main() {
+	server.bindAsync(
+		bindAddress,
+		grpcLibrary.ServerCredentials.createInsecure(),
+		(error, _port) => {
+			if (error) {
+				console.error("Server binding error:", error);
+				return;
+			}
+			console.log(`Server running at ${bindAddress}`);
+			// server.start();
+		}
+	);
+}
+main();
