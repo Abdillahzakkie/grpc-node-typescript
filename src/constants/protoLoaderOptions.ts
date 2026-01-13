d23a40074cd9d7af71daad7ebcbc95d7457279d7
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2026-01-13 18:00:44 +0100

    add protoLoaderOptions constants for gRPC configuration

---
+++ src/constants/protoLoaderOptions.ts
@@ -0,0 +1,11 @@
+import { Options } from "@grpc/proto-loader";
+
+interface IProtoLoaderOptions extends Options {}
+
+export const protoLoaderOptions: IProtoLoaderOptions = {
+	keepCase: true,
+	longs: String,
+	enums: String,
+	defaults: true,
+	oneofs: true,
+};
