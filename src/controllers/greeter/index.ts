6da53f03a214718797e5d9555feebf06a1831e80
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2026-01-14 15:12:15 +0100

    refactor: simplify sayHello implementation by removing redundant interfaces

--- src/controllers/greeter/index.ts
+++ src/controllers/greeter/index.ts
@@ -1,23 +1,10 @@
-import * as grpcLibrary from "@grpc/grpc-js";
+import { GreeterServiceHandlers } from "../../proto/generated/greeter/GreeterService";
 
-export interface HelloRequest {
-	first_name: string;
-	last_name: string;
-}
-
-export interface HelloResponse {
-	message: string;
-}
-
-export const sayHello: grpcLibrary.handleUnaryCall<
-	HelloRequest,
-	HelloResponse
-> = (
-	call: grpcLibrary.ServerUnaryCall<HelloRequest, HelloResponse>,
-	callback: grpcLibrary.sendUnaryData<HelloResponse>
+export const sayHello: GreeterServiceHandlers["SayHello"] = (
+	call,
+	callback
 ) => {
-	const firstName = call.request.first_name;
-	const lastName = call.request.last_name;
+	const { firstName, lastName } = call.request;
 	const message = `Hello, ${firstName} ${lastName}!`;
 	callback(null, { message });
 };
