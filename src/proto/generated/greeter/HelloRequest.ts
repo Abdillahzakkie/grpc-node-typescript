8b82339a463cea12a3e05bf0bcb31b8361f41373
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2026-01-14 15:12:40 +0100

    feat: add HelloRequest interface and output definition

---
+++ src/proto/generated/greeter/HelloRequest.ts
@@ -0,0 +1,12 @@
+// Original file: src/proto/greeter.proto
+
+
+export interface HelloRequest {
+  'firstName'?: (string);
+  'lastName'?: (string);
+}
+
+export interface HelloRequest__Output {
+  'firstName': (string);
+  'lastName': (string);
+}
