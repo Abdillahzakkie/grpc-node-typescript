8ba9dde98233b7197f88a22238c76f1941612e2f
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2026-01-14 15:12:08 +0100

    refactor: remove packageName parameter and simplify protoPackageLoader function

--- src/constants/protoPackageLoader.ts
+++ src/constants/protoPackageLoader.ts
@@ -4,21 +4,18 @@ import { resolve } from "node:path";
 import { protoLoaderOptions } from "./protoLoaderOptions";
 import { BASE_PROTO_FOLDER } from "./environments";
 
-export default function protoPackageLoader<T = any>({
+export default function protoPackageLoader<T>({
 	basePath = BASE_PROTO_FOLDER,
 	path,
-	packageName,
 }: {
 	basePath?: string;
 	path: string;
-	packageName: string;
 }): T {
 	const fullPath = resolve(__dirname, "../../src", basePath, path);
 
 	const packageDefinition = protoLoader.loadSync(fullPath, protoLoaderOptions);
 
 	const protoDescriptor = grpcLibrary.loadPackageDefinition(packageDefinition);
-
-	const resolvedPackage = protoDescriptor[packageName] as T;
+	const resolvedPackage = protoDescriptor as T;
 	return resolvedPackage;
 }
