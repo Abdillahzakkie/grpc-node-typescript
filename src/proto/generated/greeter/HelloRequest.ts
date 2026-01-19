6e11737e5b164545236f752accce43f310895149
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2026-01-19 12:27:46 +0100

    feat: create HelloRequest interface and output types for gRPC

---
+++ src/proto/generated/greeter/HelloRequest.ts
@@ -0,0 +1,11 @@
+// Original file: src/proto/greeter.proto
+
+export interface HelloRequest {
+	firstName?: string;
+	lastName?: string;
+}
+
+export interface HelloRequest__Output {
+	firstName: string;
+	lastName: string;
+}
