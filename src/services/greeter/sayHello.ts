9a806db79d32546b8b7042f783ec1c887e911262
Abdullah Zakariyya <66228619+Abdillahzakkie@users.noreply.github.com>
2026-01-19 12:28:02 +0100

    feat: update sayHello function to return HelloResponse__Output type

--- src/services/greeter/sayHello.ts
+++ src/services/greeter/sayHello.ts
@@ -1,10 +1,12 @@
+import type { HelloResponse__Output } from "../../proto/generated/greeter/HelloResponse";
+
 export function sayHello({
 	firstName,
 	lastName,
 }: {
 	firstName: string;
 	lastName: string;
-}) {
+}): HelloResponse__Output {
 	const message = `Hello, ${firstName} ${lastName}! DragonLord greets you.`;
 	return { message };
 }
